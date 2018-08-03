2359d4b5e8f5785599f102b1f40f17676b125ab7
Gdruzkina <g.druzkina@gmail.com>
2018-08-03 18:45:24 +0300

    HomeWork2

--- src/main/java/BadCodeExample.java
+++ src/main/java/BadCodeExample.java
@@ -1,11 +1,9 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
-//import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 import static org.openqa.selenium.Keys.ENTER;
@@ -21,11 +19,25 @@ public class BadCodeExample {
         sleep(3000);//wait for 3 sec
         //Verify that results list contacts 10 elemwnts
         List<WebElement> searchResults = driver.findElements(By.xpath("//div[@class='srg']/div[@class='g']")); //создана переменная? которая подсчитает количество елементов
-        System.out.print("Results count: "+searchResults.size()); // вывести результат теста обьеденили две строки
-        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        for (WebElement searchResult: searchResults) { //поиск в цикле нашего значениея и выведение его результатов
+        System.out.println("Результат: " + searchResults.size());// вывести результат теста обьеденили две строки
+        if (searchResults.size() >= 10) {
+            System.out.println(" Results count is correct");
+        } else {
+            System.out.println(" Results count is incorect");
+        }
+        for (WebElement searchResult : searchResults) { //поиск в цикле нашего значениея и выведение его результатов
             String serchResultText = searchResult.getText();
-            System.out.print(serchResultText);
+            //if
+            System.out.println(serchResultText);
+            if (serchResultText.contains("Selenium")) {
+                System.out.println("Searchterm found");
+            }
+            else {
+                System.out.println("Searchterm not found");
+            }
+//            WebElement userName = driver.findElements(By.xpath("//input"));
+
+
         }
 //метод контейнс последовательности
         //
