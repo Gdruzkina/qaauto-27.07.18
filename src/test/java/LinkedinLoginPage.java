f5193223953233fa54899c56d4901d831a41ccac
Gdruzkina <g.druzkina@gmail.com>
2018-08-13 22:52:30 +0300

    Lesson4

--- src/test/java/LinkedinLoginPage.java
+++ src/test/java/LinkedinLoginPage.java
@@ -1,67 +1,36 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 
 import static java.lang.Thread.sleep;
 
 public class LinkedinLoginPage {
-    // нужно подключить фреймворк-для этого следующая строка testng ето сторонняя утилита добавляется в dependencies,
-    // если есть 10 кейсот то tng помогает запустить некоторые из них
-    WebDriver driver;
-    WebElement userEmailField;
-    WebElement userPasswordField;
-    WebElement signInButton;
 
+    private WebDriver driver;
+
+    private WebElement userEmailField;
+    private WebElement userPasswordField ;
+    private WebElement signInButton ;
 
     public LinkedinLoginPage(WebDriver driver) {
         this.driver = driver;
         initElements();
     }
-
-    public void initElements() {
-        userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+    private void initElements(){
+        userEmailField = driver.findElement(By.xpath("//*[@id='login-email']"));
         userPasswordField = driver.findElement(By.xpath("//*[@id='login-password']"));
         signInButton = driver.findElement(By.xpath("//*[@id='login-submit']"));
-
     }
-
-    public void login(String userName, String userPass){
-        userEmailField.sendKeys(userName); //"galdruzk@gmail.com");
-        userPasswordField.sendKeys(userPass); //"Parol123!");
-        //sleep(3000);
+    public void login(String userEmail, String userPass){
+        userEmailField.sendKeys(userEmail);
+        userPasswordField.sendKeys(userPass);
         signInButton.click();
-        //sleep(5000);
-    }
-
-
-    public static class LinkedinDasboardPage {
-        WebDriver driver;
-        String checKURL;
-        String pageTitle;
-        WebElement profileNavigationItem;
-
-        public LinkedinDasboardPage(WebDriver driver) {
-            this.driver = driver;
-            elementsDoshboard();
-        }
-
-        public void elementsDoshboard() {
-            checKURL = driver.getCurrentUrl();//обьявляем переменную
-            pageTitle = driver.getTitle();//Обьявляем переменную
-            profileNavigationItem = driver.findElement(By.xpath("//*[@id='profile-nav-item']"));// обьявляем вебелемент
-
-        }
-
-        public void chekElements() {
-            Assert.assertEquals(checKURL, "https://www.linkedin.com/feed/", "1.You haven't correct login URL");// сравниваем  значения с переменной и ОР
-            //System.out.println("Results " + checKURL);
-            Assert.assertEquals(pageTitle, "LinkedIn", "2.You haven't correct home page title");//сравниваем значение из переменной и ОР
-            //System.out.println("Results " + pageTitle);
-            //Assert.assertEquals(profileNavigationItem.isDisplayed(), true);// тоже самое что и сторока ниже
-            Assert.assertTrue(profileNavigationItem.isDisplayed(), "'profileNavigationItem' is not displayed on Home page");// тожесамое  что и предыдущая строка
-
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
+
 }
--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -1,3 +1,4 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,72 +8,43 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 import static java.lang.Thread.sleep;
-import static org.openqa.selenium.Keys.ENTER;
+
 
 public class LinkedinLoginTest {
-    WebDriver driver;//обьявили переменную
+    WebDriver driver;
+    LinkedinLoginPage linkedinLoginPage;
 
     @BeforeMethod
-    public void beforeMetod(){
-        driver = new FirefoxDriver();//присваиваем переменной значение
+    public void beforeMethod() {
+        driver = new FirefoxDriver();
         driver.get("https://www.linkedin.com/");
+        linkedinLoginPage = new LinkedinLoginPage(driver);
     }
+
     @AfterMethod
-    public void afterMetod(){
+    public void afterMethod() {
         driver.close();
     }
 
-    @Test //анотация - метка для компиляторов для выполнения кода.
-    public void successfulLoginTest(){ //название нового метода
+    @Test
+    public void successfulLoginTest() throws InterruptedException {
 
-        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(driver);//
-        linkedinLoginPage.login("galdruzk@gmail.com","Parol123!");
-        //sleep(5000);
-    //HT v1
-        LinkedinLoginPage.LinkedinDasboardPage linkedinDasboardPage = new LinkedinLoginPage.LinkedinDasboardPage(driver);
-        linkedinDasboardPage.chekElements();
-
+        linkedinLoginPage.login("galdruzk@gmail.com", "Parol123!");
+        LinkedinHomePage linkedinHomePage = new LinkedinHomePage(driver);
+        sleep(3000);
+        Assert.assertTrue(linkedinHomePage.isLoaded(), "Home page is not loaded");
     }
 
     @Test
-    public void homeTask (){
+    public void negativeLoginTest() {
 
-        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(driver);//
-        linkedinLoginPage.login("galdruzk@gmail.com","Parol123!");
-
-       // HT v2
-        LinkedinHomePage linkedinHomePage= new LinkedinHomePage(driver);
-        linkedinHomePage.isProfileNavigatioItemDisplayed();
-    }
-
-    @Test //можно сразу задать структуру, количество тестов, которые я планирую выполнять
-    public void negatiLoninTest() throws InterruptedException {
-
-
-        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(driver);//
-        linkedinLoginPage.login("galdruk@gmail.com","Par23!");
-        sleep(5000);
-
-        WebElement alertBox = driver.findElement(By.xpath("//*[@role='alert']"));
-        Assert.assertEquals(alertBox.getText(),"При заполнении формы были допущены ошибки. " +
-                "Проверьте и исправьте отмеченные поля.","Alertbox has incorrect message");
-        //проверка. проверка полного соответствия (текст и переменной)
-
+        linkedinLoginPage.login("gasd", "Paro!");
+        //LinkedinLoginSubmitPage linkedinLoginSubmitPage = new LinkedinLoginSubmitPage(driver);
+        //Assert.assertEquals(linkedinLoginSubmitPage.getAlertBoxText(), "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.", "Alert box has incorrect message");
 
     }
+
 
 }
 
-//find one webelement
-//String checKURL = driver.getCurrentUrl();//обьявляем переменную
-//Assert.assertEquals(checKURL, "https://www.linkedin.com/feed/" ,"1.You haven't correct login URL" );// сравниваем  значения с переменной и ОР
-//System.out.println("Results " +checKURL);
-//String pageTitle = driver.getTitle();//Обьявляем переменную
-//Assert.assertEquals(pageTitle, "LinkedIn" ,"2.You haven't correct home page title" );//сравниваем значение из переменной и ОР
-//System.out.println("Results " + pageTitle);
-//WebElement profileNavigationItem = driver.findElement(By.xpath("//*[@id='profile-nav-item']"));// обьявляем вебелемент
-//Assert.assertEquals(profileNavigationItem.isDisplayed(), true);// тоже самое что и сторока ниже
-//Assert.assertTrue(profileNavigationItem.isDisplayed(),"'profileNavigationItem' is not displayed on Home page");// тожесамое  что и предыдущая строка
